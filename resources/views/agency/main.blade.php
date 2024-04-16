61a5cf5774f7156fdb1c8e152995aee4219c3ecd
azbagas <azharbagaskara1@gmail.com>
2024-04-16 22:48:08 +0700

    fix bulk delete

--- resources/views/agency/main.blade.php
+++ resources/views/agency/main.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
-@if (session()->has('success'))
+@if (session()->has('success') || request()->has('delete_success'))
     <div class="flex sm:ml-72 sm:mr-8 mt-4 mitems-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
         <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
         </svg>
         <span class="sr-only">Info</span>
         <div>
-        <span class="font-medium">{{ session('success') }}
+        <span class="font-medium">{{ session('success', 'Data berhasil dihapus') }}</span>
         </div>
     </div>
 @endif
+
+{{-- Untuk bulk delete --}}
+<div id="error-alert"></div>
 
 <div class="flex flex-col sm:ml-64 ">
     <div class="overflow-x-auto">
@@ -48,7 +51,7 @@
                         <tr class="hover:bg-gray-100 dark:hover:bg-gray-700" id="{{ $search }}_ids{{ $agency->id }}">
                             <td class="w-4 p-4">
                                 <div class="flex items-center">
-                                    <input id="" aria-describedby="checkbox-1" name="ids[]" type="checkbox" class="checkbox_ids w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" value="{{ $agency->id }}">
+                                    <input id="" aria-describedby="checkbox-1" name="ids" type="checkbox" class="checkbox_ids w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" value="{{ $agency->id }}">
                                     <label for="checkbox" class="sr-only">checkbox</label>
                                 </div>
                             </td>
