4b03d42c96ffd04570d4b11ef14fc6d97e52487f
azbagas <azharbagaskara1@gmail.com>
2024-05-10 09:25:58 +0700

    fix photo path in fregistrasi

--- resources/views/forms/main.blade.php
+++ resources/views/forms/main.blade.php
@@ -114,7 +114,7 @@
                                         </div>
                                     </td>
                                     <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                        <img class="w-10 h-10 rounded-full" src="{{ asset($talent->photo) }}" alt="{{ $talent->name }}">
+                                        <img class="w-10 h-10 rounded-full" src="{{ asset('storage/' . $talent->photo) }}" alt="{{ $talent->name }}">
                                         <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
                                             <div class="text-base font-semibold text-gray-900 dark:text-white">{{ $talent->name }}</div>
                                             <div class="text-sm font-normal text-gray-500 dark:text-gray-400">{{ $talent->email }}</div>
