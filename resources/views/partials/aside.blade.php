7c5c6d1a8a8088afac9a2cecce172169a8ff4266
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2024-05-20 21:22:51 +0700

    fix feature

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -1,6 +1,7 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
+        @can('view dashboard')
         <li>
             <span class="text-sm">Beranda</span>
             <a href="/fdashboard" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ Request::is('fdashboard') ? 'bg-gray-200 hover:text-gray-600' : '' }}">
@@ -11,6 +12,7 @@
                 <span class="ms-4">Dashboard</span>
             </a>
         </li>
+        @endcan
         <li>
             <span class="text-sm">Administrator</span>
             <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
