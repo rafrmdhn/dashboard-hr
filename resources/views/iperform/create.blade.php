97e11acdfd0ab4514d64149bffb603462db872a4
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2024-05-20 21:40:30 +0700

    fix checkbox and fix permission

--- resources/views/iperform/create.blade.php
+++ resources/views/iperform/create.blade.php
@@ -5,7 +5,7 @@
             <!-- Modal header -->
             <div class="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-700">
                 <h3 class="text-xl font-semibold dark:text-white">
-                    Tambah Kinerja Staff
+                    Tambah Kinerja Intern
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white" data-modal-toggle="add-user-modal">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
@@ -17,7 +17,7 @@
                     @csrf
                     <div class="grid grid-cols-6 gap-6">     
                         <div class="col-span-6">
-                            <label for="intern_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Staff</label>
+                            <label for="intern_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Intern</label>
                             <select name="intern_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                                 <option value="" selected>Pilih Intern</option>
                                     @foreach ($interns as $intern)
--- resources/views/iperform/edit.blade.php
+++ resources/views/iperform/edit.blade.php
@@ -1,6 +1,6 @@
 <button type="button" data-modal-target="edit-data-modal-{{ $performance->id }}" data-modal-toggle="edit-data-modal-{{ $performance->id }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-    Edit staff
+    Edit Intern
 </button>
 
 <div class="fixed left-0 right-0 z-50 items-center justify-center hidden overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full" id="edit-data-modal-{{ $performance->id }}">
@@ -10,7 +10,7 @@
             <!-- Modal header -->
             <div class="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-700">
                 <h3 class="text-xl font-semibold dark:text-white">
-                    Edit Staff
+                    Edit Intern
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white" data-modal-toggle="edit-data-modal-{{ $performance->id }}">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
@@ -23,9 +23,9 @@
                     @csrf
                     <div class="grid grid-cols-6 gap-6">
                         <div class="col-span-6">
-                            <label for="intern_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Staff</label>
+                            <label for="intern_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Intern</label>
                             <select name="intern_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                                <option value="" selected>Pilih Staff</option>
+                                <option value="" selected>Pilih Intern</option>
                                     @foreach ($interns as $intern)
                                         @if (old('intern_id', $intern->id) == $intern->id)
                                             <option value="{{ $intern->id }}" selected>{{$intern->name}}</option>
